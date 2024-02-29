119f93d406f3a3870ee803bae17b60e6821e884e
merndevreza <merndevreza@gmail.com>
2024-02-29 17:04:36 +0600

    Dynamic Field by using useFieldArray

--- src/components/Forms/Registration/RegistrationForm.jsx
+++ src/components/Forms/Registration/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import Field from "../Field";
 import Fieldset from "../Fieldset";
 
@@ -6,11 +6,19 @@ const RegistrationForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }, 
+    formState: { errors },
+    control,
   } = useForm();
+
+  const { fields, append, remove } = useFieldArray({
+    name: "socials",
+    control,
+  });
+
   const submitForm = (formData) => {
-   console.log(formData); 
- };
+    console.log(formData);
+  };
+ 
   return (
     <form
       onSubmit={handleSubmit(submitForm)}
@@ -32,11 +40,11 @@ const RegistrationForm = () => {
         <Field label="Age" error={errors.age}>
           <input
             {...register("age", {
-               required: "Age is required",
-               max: {
-                  value: 100,
-                  message: "Age must be between 0 and 100",
-                },
+              required: "Age is required",
+              max: {
+                value: 100,
+                message: "Age must be between 0 and 100",
+              },
             })}
             className={`border-2 p-2 rounded ${
               errors.age ? "border-red-500" : "border-green-400 "
@@ -77,15 +85,48 @@ const RegistrationForm = () => {
             placeholder="Enter password"
           />
         </Field>
-        <Field>
-          <button
-            type="submit"
-            className="py-3 text-xl bg-green-600 rounded font-semibold text-white"
-          >
-            Register
-          </button>
-        </Field>
       </Fieldset>
+      <fieldset>
+        <h3>Add Social Handles</h3>
+        {fields &&
+          fields.map((field, index) => (
+            <div className="flex justify-between " key={field.id}>
+              <Field label="Social Name">
+                <input
+                  {...register(`socials[${index}].name`)}
+                  className="border-2 p-2 rounded border-green-400"
+                  type="text"
+                  name={`socials[${index}].name`}
+                  id={`socials[${index}].name`}
+                />
+              </Field>
+              <Field label="Social URL">
+                <input
+                  {...register(`socials[${index}].url`)}
+                  className="border-2 p-2 rounded border-green-400"
+                  type="text"
+                  name={`socials[${index}].url`}
+                  id={`socials[${index}].url`}
+                />
+              </Field>
+              <button onClick={()=>remove(index)} className="p-2 bg-red-600 m-2 ">X</button>
+            </div>
+          ))}
+        <button
+          onClick={() => append({ name: "", url: "" })}
+          className="px-3 py-1 bg-orange-500 rounded"
+        >
+          @ Add a social handle
+        </button>
+      </fieldset>
+      <Field>
+        <button
+          type="submit"
+          className="py-3 text-xl bg-green-600 rounded font-semibold text-white"
+        >
+          Register
+        </button>
+      </Field>
       <div className="text-xl font-semibold text-red-600 text-center mt-3">
         {errors?.root?.random?.message}
       </div>
